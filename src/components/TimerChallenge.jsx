521d1fdf0d0de523f17c4e51fc0b52ba1e6fc025
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-05 17:53:09 +0530

    Add timer to App.jsx and state & ref to store time

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from "react";
+
+//let timer; // using outside of the component so useState not re-evaluated
+
+const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const [timerStarted, setTimerStarted] = useState(false); // [1
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timeExpired && <p>You Lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Clicking!!
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running out" : "Time inactive"}
+      </p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
